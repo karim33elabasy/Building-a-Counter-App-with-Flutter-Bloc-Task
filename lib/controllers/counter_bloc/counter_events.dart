8de6f670764e8a2159de6acfd1f2f234d7a86cd8
karim <karim33elabasy@gmail.com>
2025-01-24 15:22:11 +0200

    first commit

--- lib/controllers/counter_bloc/counter_events.dart
+++
@@ -1,5 +0,0 @@
-abstract class CounterEvents{}
-
-class AddingNumber extends CounterEvents{}
-
-class ResettingNumber extends CounterEvents{}
--- lib/controllers/theme_bloc/theme_events.dart
+++
@@ -1,6 +0,0 @@
-abstract class ThemeEvents{}
-
-class ThemeChange extends ThemeEvents{
-  final bool isDark;
-  ThemeChange({required this.isDark});
-}
